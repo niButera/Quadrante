35fd2651f8fb9ddf19e1ff724a2c5ed9ce7ac346
niButera <nikolasxp683@gmail.com>
2023-05-27 21:17:08 -0300

    Programa que determina se as cordenadas X e Y estão no quandrante I,II,III, IV, origem ou na linha

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+
+Console.WriteLine(@"     --- Quadrante Cartesiano ---
+Forneça as coordenadas (apenas números)");
+
+Console.WriteLine();
+
+Console.Write("Coordenada X: ");
+int X = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Coordenada Y: ");
+int Y = Convert.ToInt32(Console.ReadLine()!);
+
+if (X == 0 && Y == 0) 
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica na origem");
+}
+
+else if (X > 0 && Y > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica no quadrante I");
+}
+
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica no quadrante II");
+}
+
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y})fica no quadrante III");
+}
+
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica no quadrante IV");
+}
+
+else if (X == 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica no eixo X");
+}
+
+else if (Y == 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"O ponto ({X}, {Y}) fica no eixo Y");
+}
+
+Console.WriteLine();
